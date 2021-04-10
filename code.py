75b1c83e13bad0f40d2c263c7be04fb739ae7ff2
PrachiJukaria <prachijukaria1408@gmail.com>
2021-04-10 23:01:45 +0530

    Initial commit

---
+++ code.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+#pyautogui.write('Hello world!', interval=0.25
+print("hii")
+pyautogui.keyDown("h")
+pyautogui.keyDown("h")
+pyautogui.keyDown("h")
+#
+# if __name__=="__main__":'''
